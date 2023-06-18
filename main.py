00d5e541600e4ec382afd0d1ccfdf664bcaefb31
unknown <aishvaryasudha@gmail.com>
2023-06-18 12:49:06 +0530

    Basic Version
    basic version h=of the code has been added, changes to be made

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import requests
+from InquirerPy import prompt
+import json
+
+question1 = [
+    {
+        "type": "list",
+        "message": "What's the currency you want to convert?",
+        "choices": ["INR", "USD", "EUR","JPY" ,"CNY","KRW","SGD","NZD","CAD","ZAR"]
+    },
+    {
+        "type": "list",
+        "message": "What's the currency you want to convert into?",
+        "choices": ["INR", "USD", "EUR","JPY" ,"CNY","KRW","SGD","NZD","CAD","ZAR"]
+    }
+]
+result = prompt(question1)
+cur1=result[0]
+cur2=result[1]
+
+
+amount=float(input("Enter the amount to convert :"))
 
 url = 'https://api.exchangerate.host/convert'
-params = {'from': 'USD', 'to': 'EUR'}
+params = {'from': cur1, 'to': cur2}
 
 response = requests.get(url, params=params)
 data = response.json()
 
 if 'result' in data:
-    conversion_rate = data['result']
-    print(f"The conversion rate from USD to EUR is: {conversion_rate}")
+    rate = data['result']
+    print("The conversion rate from",cur1,"to",cur2, "is:", rate)
 else:
     print("Failed to retrieve conversion rate")
+
+print("Therefore the amount after conversion is:",amount*rate)
+
